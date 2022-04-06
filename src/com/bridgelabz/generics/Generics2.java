b0fcadb2a9dedabe166106997f45fe6c8e1c96bd
sunilkumarojha <100593013+sunilkumarojha@users.noreply.github.com>
2022-04-06 22:19:49 +0530

    Max float out of three

--- src/com/bridgelabz/generics/Generics2.java
+++ src/com/bridgelabz/generics/Generics2.java
@@ -1,2 +1,20 @@
 package com.bridgelabz.generics;
 
+public class Generics2 {
+
+    public static Integer compareTo(Integer a, Integer b, Integer c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Welcome to Generics program.
+
+        System.out.println("Maximun number out of three is: " + compareTo(5, 7, 8));
+    }
+}
